984e192560070a638e2ee64c5af47cf9c96015fa
Yuda <Yuda@gmail.com>
2023-04-14 10:13:48 +0700

    Add utils

---
+++ src/pages/AddCourse/styles.js
@@ -0,0 +1,12 @@
+import { Container } from "react-bootstrap";
+import styled from "styled-components";
+
+export const StyledContainer = styled(Container)`
+margin-top: 30px;
+margin-bottom:30px;
+text-allign: left;
+`
+
+export const StyledTitle = styled.h1`
+margin-bottom: 20px;
+`
